870ee8a877c4bbda129778b4c3de007eb57dfecc
Muhamed Elsayed <mohamedelsayed100100100@gmail.com>
2025-10-17 06:08:18 +0300

    Working with Github

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
+class SocialiteController extends Controller
+{
+    public function login()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function redirect()
+    {
+        $SocialiteUser = Socialite::driver('github')->user();
+        $user = User::updateOrCreate([
+            'provider_id' => $SocialiteUser->getId(),
+        ], [
+            'name' => $SocialiteUser->getName(),
+            'email' => $SocialiteUser->getEmail(),
+
+        ]);
+
+
+        // auth User
+        Auth::login($user, true);
+
+        // redirect to dashboard
+        return to_route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::prefix('github')->name('socialite.')->controller(SocialiteController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::get('/redirect', 'redirect')->name('redirect');
+});
